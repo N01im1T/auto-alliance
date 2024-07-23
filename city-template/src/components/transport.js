e448f69977d71fc163e47ce582589224fa2e23d9
N01im1T <lavrinait.v@yandex.ru>
2024-07-23 16:08:35 +1000

    feat: add buttons -Added buttons styles -Added some buttons handlers -Refactored control buttons for transport slider

--- city-template/src/components/transport.js
+++ city-template/src/components/transport.js
@@ -2,14 +2,39 @@ import Glide from "@glidejs/glide";
 import "@glidejs/glide/dist/css/glide.core.css";
 
 const transport = () => {
-  const transportSlider = new Glide('.glide', {
-    type: 'slider',
+  const buttons = document.querySelectorAll(".btn-control");
+
+  function updateActiveButton(index) {
+      buttons.forEach((button, i) => {
+          if (i === index) {
+              button.classList.add("active");
+          } else {
+              button.classList.remove("active");
+          }
+      });
+  };
+
+  const transportSlider = new Glide(".glide", {
+    type: "slider",
     startAt: 0,
-    focusAt: 'center',
+    focusAt: "center",
     perView: 1,
   });
 
   transportSlider.mount();
+
+  transportSlider.on("run.after", () => {
+      const activeIndex = transportSlider.index;
+      updateActiveButton(activeIndex);
+  });
+
+  buttons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+          updateActiveButton(index);
+      });
+  });
+
+  updateActiveButton(transportSlider.index);
 };
 
 export default transport;
--- city-template/src/main.js
+++ city-template/src/main.js
@@ -3,10 +3,12 @@ import "../src/styles/main.css";
 import header from "./components/header.js";
 import transport from "./components/transport.js";
 import footer from "./components/footer.js";
+import buttons from "./components/buttons.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   "use strict";
   header();
   transport();
   footer();
+  buttons();
 });
