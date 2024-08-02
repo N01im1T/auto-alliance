682f8aed70e1e0408c899e77a44bccd9779f4d1c
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-08-02 14:13:06 +1000

    feat: add mobile header -Added scripts for toggle button -Added scripts for mobile buttons / mobile menu -Added styles for mobile verson header

--- city-template/src/components/buttons.js
+++ city-template/src/components/buttons.js
@@ -1,5 +1,39 @@
 const buttons = () => {
+  const toggleBtn = document.getElementById("toggle-btn");
+  const ruBtn = document.getElementById("ru");
+  const enBtn = document.getElementById("en");
 
+  // Check site language
+  document.addEventListener("DOMContentLoaded", function () {
+    const enSelected = enBtn.getAttribute("aria-selected") === "true";
+    if (enSelected) {
+      document.querySelector(".indicator").style.transform = "translateX(0)";
+    }
+  });
+
+  function switchLanguage(language) {
+    if (language === "ru") {
+      window.location.href = "/city-template/public/ru/index.html";
+    } else if (language === "en") {
+      window.location.href = "/city-template/public/en/index.html";
+    }
+  }
+
+  toggleBtn.addEventListener("click", function () {
+    const ruSelected = ruBtn.getAttribute("aria-selected") === "true";
+
+    if (ruSelected) {
+      ruBtn.setAttribute("aria-selected", "false");
+      enBtn.setAttribute("aria-selected", "true");
+      document.querySelector(".indicator").style.transform = "translateX(0)";
+      switchLanguage("en");
+    } else {
+      ruBtn.setAttribute("aria-selected", "true");
+      enBtn.setAttribute("aria-selected", "false");
+      document.querySelector(".indicator").style.transform = "translateX(100%)";
+      switchLanguage("ru");
+    }
+  });
 };
 
 export default buttons;
